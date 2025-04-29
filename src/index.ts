b078fb2dcc35408fa6ae3185bc61b5cce58546a9
Paul Gailey Alburquerque <paul@everywoah.com>
2025-04-29 20:49:28 +0200

    syntax typo fixed
    ./geoLocationMiddleware.js caused issues so .js deleted

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import crypto from 'crypto';
 import axios from 'axios';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import geoLocationMiddleware from './geoLocationMiddleware.js'; // <-- Import the geolocation middleware
+import geoLocationMiddleware from './geoLocationMiddleware'; // <-- Import the geolocation middleware
 
 declare module '@augmentos/sdk' {
   interface TpaSession {
